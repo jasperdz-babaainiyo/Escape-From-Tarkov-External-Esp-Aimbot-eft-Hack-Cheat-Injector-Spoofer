99ed360f31a1c1961fab75f89463f8538702b0ca
notSMods <108301126+notSMods@users.noreply.github.com>
2022-06-30 23:54:14 +0400

    Add files via upload

---
+++ Escape From Tarkov External Esp Aimbot eft Hack Cheat Injector Spoofer/MainMenu.cs
@@ -0,0 +1,261 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace EFT_Pak_Loader
+{
+    public partial class MainMenu : Form
+    {
+        string hackPath = @"C:\Program Files\Windows Driver\platform_x64";
+        string originalPaksCopyPath = @"C:\Program Files\Windows Driver\core_x64";
+        string tarkovDataPath;
+
+        Stopwatch sw = new Stopwatch();
+        WebClient wc;
+
+        public MainMenu()
+        {
+            InitializeComponent();
+
+            tarkovDataPath = textBoxGameFolder.Text + "\\EFT\\EscapeFromTarkov_Data";
+
+            if (Directory.Exists(hackPath))
+            {
+                groupBoxDownload.Enabled = false;
+                lblProgressBar.Text = "Cheat downloaded!";
+            }
+            else
+            {
+                groupBoxGameFolder.Enabled = false;
+                groupBoxFeatures.Enabled = false;
+            }
+        }
+
+        // -----------------------------------------------------------------------------------------------------
+
+        // ----- GroupBox Download -----
+        #region Download Region
+        // Download all files button
+        private void btnDownload_Click(object sender, EventArgs e)
+        {            
+            using (wc = new WebClient())
+            {
+                wc.DownloadFileCompleted += new AsyncCompletedEventHandler(Completed);
+                wc.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressChanged);
+
+                Directory.CreateDirectory(hackPath);                
+
+                wc.DownloadFileAsync(new Uri("ENTER_YOUR_URL_HERE"), hackPath);   // <- CHANGEME: Follow instructions
+            }            
+        }
+
+        // The event that will fire whenever the progress of the WebClient is changed
+        private void ProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {           
+            progressBar1.Value = e.ProgressPercentage;
+
+            lblProgressBar.Text = e.ProgressPercentage.ToString() + "%";
+        }
+
+        // The event that will trigger when the WebClient is completed
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            sw.Reset();
+
+            if (e.Cancelled == true)
+            {
+                MessageBox.Show("Cheat download has been cancelled!");
+                lblProgressBar.Text = "Waiting for download...";
+            }
+            else
+            {
+                MessageBox.Show("Cheat download completed!");
+                lblProgressBar.Text = "Cheat downloaded!";
+                groupBoxGameFolder.Enabled = true;
+                groupBoxFeatures.Enabled = true;
+            }
+        }
+        #endregion
+
+        // -----------------------------------------------------------------------------------------------------
+
+        // ----- GroupBox Game folder -----
+        private void textBoxGameFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Select the 'Battlestate Games' folder!";
+
+            if (fbd.ShowDialog() == DialogResult.OK && Directory.Exists(fbd.SelectedPath + "\\EFT\\EscapeFromTarkov_Data"))
+            {
+                MessageBox.Show("BattleState Games Folder:\n" + fbd.SelectedPath);
+                textBoxGameFolder.Text = fbd.SelectedPath;
+            }
+            else
+                MessageBox.Show("Thats the wrong folder! Try again..");
+        }
+
+        // -----------------------------------------------------------------------------------------------------
+
+        // ----- GroupBox Features ------
+        #region Install Button
+        // Install the choosen hacks
+        private void btnInstall_Click(object sender, EventArgs e)
+        {        
+            // Check if right game folder path
+            if (!Directory.Exists(textBoxGameFolder.Text + "\\EFT\\EscapeFromTarkov_Data"))
+            {
+                MessageBox.Show("Wrong Game Folder selected!");
+                return;
+            }
+
+            #region Save all (!) original files first
+            // Save the original player files first
+            if (!Directory.Exists(originalPaksCopyPath + "\\player"))
+            {
+                Directory.CreateDirectory(originalPaksCopyPath + "\\player");
+            }
+
+            foreach (var file in Directory.GetFiles(tarkovDataPath + "THE_RIGHT_FOLDER_1")) // CHANGEME: Enter the right folder
+            {
+                File.Copy(file, file.Replace(tarkovDataPath + "THE_RIGHT_FOLDER_1", originalPaksCopyPath + "\\player"), true); // CHANGEME: Enter the right folder
+            }
+
+            // Save the original item files first
+            if (!Directory.Exists(originalPaksCopyPath + "\\item"))
+            {
+                Directory.CreateDirectory(originalPaksCopyPath + "\\item");
+            }
+
+            foreach (var file in Directory.GetFiles(tarkovDataPath + "THE_RIGHT_FOLDER_2")) // CHANGEME: Enter the right folder
+            {
+                File.Copy(file, file.Replace(tarkovDataPath + "THE_RIGHT_FOLDER_2", originalPaksCopyPath + "\\item"), true); // CHANGEME: Enter the right folder
+            }
+
+            // Save the original sound files first
+            if (!Directory.Exists(originalPaksCopyPath + "\\sound"))
+            {
+                Directory.CreateDirectory(originalPaksCopyPath + "\\sound");
+            }
+
+            foreach (var file in Directory.GetFiles(tarkovDataPath + "THE_RIGHT_FOLDER_3")) // CHANGEME: Enter the right folder
+            {
+                File.Copy(file, file.Replace(tarkovDataPath + "THE_RIGHT_FOLDER_3", originalPaksCopyPath + "\\sound"), true); // CHANGEME: Enter the right folder
+            }
+            #endregion
+
+            #region Replace with the modded ones
+            // Check which features are enabled
+            if (checkBoxPlayerESP.Checked)
+            {
+                // Replace the original files with the modded ones
+                foreach (var file in Directory.GetFiles(hackPath + "\\player"))
+                {
+                    File.Copy(file, file.Replace(hackPath + "\\player", tarkovDataPath + "THE_RIGHT_FOLDER_1"), true); // CHANGEME: Enter the right folder
+                }
+            }
+
+            if (checkBoxItemESP.Checked)
+            {
+                // Replace the original files with the modded ones
+                foreach (var file in Directory.GetFiles(hackPath + "\\items"))
+                {
+                    File.Copy(file, file.Replace(hackPath + "\\items", tarkovDataPath + "THE_RIGHT_FOLDER_2"), true); // CHANGEME: Enter the right folder
+                }
+            }
+
+            if (checkBoxNoSound.Checked)
+            {
+                // Replace the original files with the modded ones
+                foreach (var file in Directory.GetFiles(hackPath + "\\sound"))
+                {
+                    File.Copy(file, file.Replace(hackPath + "\\sound", tarkovDataPath + "THE_RIGHT_FOLDER_3"), true); // CHANGEME: Enter the right folder
+                }
+            }
+            #endregion
+
+            lblInstallStatus.Visible = true;
+            DialogResult result = MessageBox.Show("Ready to cheat!\n\n1. Start Tarkov via the BsgLauncher\n 2. The cheat will close automatically!", "Follow the instructions!", MessageBoxButtons.OK);
+            if (result == DialogResult.OK)
+            {
+                findEFTexe.Start();
+                lblGameStatus.Text = "Waiting for Game...";
+            }
+        }
+        #endregion
+
+        // -----------------------------------------------------------------------------------------------------
+
+        #region Wait for EFT_BE.exe Timer
+        // Wait for game to launch...
+        private void findEFTexe_Tick(object sender, EventArgs e)
+        {
+            string process = "EscapeFromTarkov_BE";
+            if (Process.GetProcessesByName(process).Length > 0)
+            {
+                lblGameStatus.Text = "GAME FOUND!!";
+                findEFTexe.Stop();
+
+                #region Copy all PAKS into EFT_Data
+                // Copy all Orig PAKS into the target (EFT_Data)
+                foreach (var file in Directory.GetFiles(originalPaksCopyPath + "\\player"))
+                {
+                    File.Copy(file, file.Replace(originalPaksCopyPath + "\\player", tarkovDataPath + "\\THE_RIGHT_FOLDER_1"), true); // CHANGEME: Enter the right folder
+                }
+
+                foreach (var file in Directory.GetFiles(originalPaksCopyPath + "\\items"))
+                {
+                    File.Copy(file, file.Replace(originalPaksCopyPath + "\\items", tarkovDataPath + "\\THE_RIGHT_FOLDER_2"), true); // CHANGEME: Enter the right folder
+                }
+
+                foreach (var file in Directory.GetFiles(originalPaksCopyPath + "\\sound"))
+                {
+                    File.Copy(file, file.Replace(originalPaksCopyPath + "\\sound", tarkovDataPath + "\\THE_RIGHT_FOLDER_3"), true); // CHANGEME: Enter the right folder
+                }
+                #endregion
+
+                foreach (var me in Process.GetProcessesByName("FirevoxBrowser"))
+                {
+                    me.Kill();
+                }
+            }
+        }
+        #endregion
+
+        // -----------------------------------------------------------------------------------------------------
+
+        #region Application Exit Override
+        // Exit Event Handler
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+
+            if (ConfirmClose() == DialogResult.Yes)
+            {
+                Dispose(true);
+
+                // ADD EACH FEATURE HERE!!
+                // Copy all Orig PAKS into the target (EFT_Data)
+                foreach (var file in Directory.GetFiles(originalPaksCopyPath))
+                {
+                    File.Copy(file, file.Replace(originalPaksCopyPath, tarkovDataPath), true);
+                }
+
+                Application.Exit();
+            }
+            else //no confirmation
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private DialogResult ConfirmClose()
+        {
+            DialogResult res = MessageBox.Show("Do you want to quit?", "Confirmation", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            return res;
+        }
+        #endregion
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# Escape-From-Tarkov-External-Esp-Aimbot-eft-Hack-Cheat-Injector-Spoofer
+###  Escape-From-Tarkov Aimbot + Esp C++ 
+![GitHub release](https://img.shields.io/github/release/ppy/osu.svg)
+![CodeFactor](https://www.codefactor.io/repository/github/ppy/osu/badge)
+![dev chat](https://discordapp.com/api/guilds/188630481301012481/widget.png?style=shield)
+![Crowdin](https://d322cqt584bo4o.cloudfront.net/osu-web/localized.svg)
+![Renovate enabled](https://img.shields.io/badge/renovate-enabled-brightgreen.svg)
+![license](https://img.shields.io/github/license/mashape/apistatus.svg)
+![Chat](https://badges.gitter.im/awesome-twitter-bots/Lobby.svg)
+
+```sh-session
+"Escape-From-Tarkov" RELEASE C++,C / AIMBOT / ESP / SPOOFER / DRIVER / Injector
+```
+***
+<p align="center">
+   <img src="https://readme-spotify-status-rho.vercel.app/api/run-spotify-status.py" alt="s4nx Playing Now" width="500" />
+<p align="center">
+
+## Information
+**External Game Project written mostly in C++ along with external libraries Internal And External projects.I started to get rid of scammers.i am developing Hack Cheat Driver Esp Aimbot Magic Bullet Driver Injector Overlay Imgui for many games.Games I've developed with hack so far Rise Online ,Apex Legends ,Bloodhunt , Call of Duty: Cold War , Call of Duty: Vanguard ,Call of Duty: Warzone/MW ,Dayz ,Dead By Daylight ,Destiny 2 ,Enlisted ,Escape From Tarkov ,Fortnite ,Game accounts ,Halo Infinite ,HyperFlick ,New Critical Hit ,New World ,Mir 4 ,Noble ,Playerunknowns Battlegrounds ,Steam ,Rainbow Siz Siege , Playerunknown's Battlegrounds, Rijin ,Rogue Company ,Rust ,Scum ,SpliteGa ,Super People ,Unleashed ,Valorant ,Spoofer ::: Buying a Hack Cheat don't be scammed, more to come**
+
+There are EFT wallhacks, aimbots, ESPs, No recoil and much more that will make you invincible. If you want to guarantee the best loot, you got it. If you want to be a winner every match, it’s all yours. If you think enemies are hiding around the corner, they won’t know what’s about to hit them.The game has several zones, including the Factory, Woods, Shoreline, the Interchange, Customs, Lab, and Reserves, and if there’s one thing they all have one thing in common, it’s non stop action. What sets this game apart from the rest is the epic survival elements mixed with its intense resource management. 
+
+**Updated Time 17/05/2022**
+
+
+
+![image](https://user-images.githubusercontent.com/105746452/169082235-b00ad770-64af-42df-af89-4cc08dc95c44.png)
+## Features
+<details>
+<summary>Features (Drop Down)</summary>
+  
+* **AIMBOT**
+  
+* **ESP**
+  
+* **SPOOFER** 
+
+* **DRIVER**
+
+*  **INJECTOR**
+  </details>
+
+**Aimbot**
+
+* Aim Prediction
+* * Aim Key
+* Head Lock
+
+**Visuals**
+
+* Player ESP
+* Scav ESP
+* Scav Boss ESP
+* Skeleton
+* Box
+* Visibility Checks
+* Health ESP
+* Player Weapon + Ammo Count
+* Player ESP Distance
+* Item ESP
+* Medical
+* Food
+* Valuables
+* Keys
+* Task
+* Ammo
+* Attachments
+* Other
+* Container ESP
+* Grenade ESP
+* Extraction ESP
+* Crosshair
+   
+**Misc**
+
+* Night Vision
+* Thermal Vision
+* No Visor
+* Infinite Stamina
+* Aim Warning
+***
+
+## Media 
+![image](https://user-images.githubusercontent.com/105746452/169082190-94ac83d4-7d45-485c-b1df-3f785d90462d.png)
+
+
+## ✨ DONATE Buy Me Coffee
+
+BTC - 144feg2TVeVjhLfXVrKvaTzu2ViX4gYv6q
+
+
+## Disclaimer
+This project is only for educational purposes. Therefore I'm not responsible for any harm/illegal activity that may happens. I made this project to learn more about reverse engineering and not to ruin the experience for other gamers. I will not be updating the offsets for this reason.This may not be exact code as the one in my hackathon.
